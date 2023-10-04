540e5fa71fc50fb05288e5731c2efc0ec66ab433
m3ghnaa <meghna.chandrababu@gmail.com>
2023-10-04 22:03:50 +0530

    add playRound func

--- app.js
+++ app.js
@@ -4,4 +4,25 @@ const randomIndex = Math.floor(Math.random() * choices.length);
 return choices[randomIndex];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const choices = ['rock', 'paper', 'scissors'];
+
+    if(playerChoice === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    }else if (
+        (playerChoice === 'rock' && computerSelection.toLowerCase() === 'scissors') || 
+        (playerChoice === 'paper' && computerSelection.toLowerCase() === 'rock') ||
+        (playerChoice === 'scissors' && computerSelection.toLowerCase() === 'paper')
+     ) {
+            return `You Win! ${playerChoice} beats ${computerSelection}`;
+        }
+    else {
+        return `You lose! ${computerSelection} beats ${playerChoice}` ;
+    }
+    
+    
+}
+
+const computerSelection = getComputerChoice()
+console.log(playRound('Rock', computerSelection));
