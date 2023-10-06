5464f4506f7880fa99e2aab432f75ea95b061101
m3ghnaa <meghna.chandrababu@gmail.com>
2023-10-06 22:46:53 +0530

    add UI - buttons, alert

--- app.js
+++ app.js
@@ -1,67 +1,80 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+let userScore = 0;
+let computerScore = 0;
+
+
+const resultsDiv = document.getElementById('results');
+const scoreDiv = document.getElementById('score');
+const gameButtons = document.querySelectorAll('.game-button');
+const resetButton = document.getElementById('reset');
+
 function getComputerChoice() {
-const choices = ['Rock', 'Paper', 'Scissors'];
-const randomIndex = Math.floor(Math.random() * choices.length);
-return choices[randomIndex];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
-    while (!['rock', 'paper', 'scissors'].includes(userChoice) ){
-        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors:").toLowerCase();
-    }
-    return userChoice;
-}
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
     const playerChoice = playerSelection.toLowerCase();
-    const choices = ['rock', 'paper', 'scissors'];
 
-    if(playerChoice === computerSelection.toLowerCase()) {
-        return "It's a tie!";
-    }else if (
-        (playerChoice === 'rock' && computerSelection.toLowerCase() === 'scissors') || 
-        (playerChoice === 'paper' && computerSelection.toLowerCase() === 'rock') ||
-        (playerChoice === 'scissors' && computerSelection.toLowerCase() === 'paper')
-     ) {
-            return `You Win! ${playerChoice} beats ${computerSelection}`;
-        }
-    else {
-        return `You Lose! ${computerSelection} beats ${playerChoice}` ;
-    }
-    
-    
-}
-
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-
-    for(let i=0; i <5; i++){
-        const userChoice = getUserChoice();
-        const computerChoice = getComputerChoice();
-        const roundResult = playRound(userChoice, computerChoice);
-
-        console.log(`Round ${i + 1}: ${roundResult}`)
-
-        if (roundResult.includes("Win")) {
-            userScore++;
-        } else if (roundResult.includes("Lose")) {
-            computerScore++;
-        }
-    }
-
-    let gameResult;
-
-    if (userScore > computerScore) {
-        gameResult = "You win the game!";
-    } else if (userScore < computerScore) {
-        gameResult = "You lose the game!";
+    if(playerChoice === computerSelection) {
+        resultsDiv.textContent = "It's a tie!";
+    } else if (
+        (playerChoice === 'rock' && computerSelection === 'scissors') ||
+        (playerChoice === 'paper' && computerSelection === 'rock') ||
+        (playerChoice === 'scissors' && computerSelection === 'paper')
+    ) {
+        resultsDiv.textContent = `You Win! ${playerChoice} beats ${computerSelection}`;
+        userScore ++;
     } else {
-        gameResult = "It's a tie!";
+        resultsDiv.textContent = `You Lose! ${computerSelection} beats ${playerChoice}`;
+        computerScore ++;
     }
 
-    console.log(`Game Over! ${gameResult}`);
+    updateScore();
+}
+
+function updateScore() {
+    scoreDiv.textContent = `User: ${userScore} | Computer: ${computerScore}`;
+    
+
+    if (userScore === 5 || computerScore === 5) {
+        endGame();
+    } 
+}
+
+function endGame() {
+    if (userScore === 5) {
+        resultsDiv.textContent = "You win the game!";
+    } else if (computerScore === 5) {
+        resultsDiv.textContent = "You lose the game!";
+    }
+
+    resetButton.style.display = 'block';
+    gameButtons.forEach((button) => {
+        button.style.display = 'none';
+    });
+
+    resetButton.addEventListener('click', ()=> {
+        alert("Game has been reset. Click Ok to play again.");
+        userScore = 0;
+        computerScore = 0;
+        scoreDiv.textContent = `User: ${userScore} | Computer: ${computerScore}`;
+        resultsDiv.textContent = '';
+        resetButton.style.display = 'none';
+
+        gameButtons.forEach((button) => {
+            button.style.display = 'block';
+        });
+    });
 }
 
 
-game();
+gameButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const playerSelection = e.target.id;
+        playRound(playerSelection);
+    });
+});
+
