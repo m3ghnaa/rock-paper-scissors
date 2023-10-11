e4ccb52e0b04e980fd72c993357c547c524126a7
m3ghnaa <meghna.chandrababu@gmail.com>
2023-10-12 01:26:36 +0530

    new ui

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const choices = ['rock', 'paper', 'scissors'];
 let userScore = 0;
 let computerScore = 0;
 
+const landingScreen = document.getElementById('landingScreen');
+const gameScreen = document.getElementById('gameScreen');
+const startGameButton = document.getElementById('startGameButton');
+
+startGameButton.addEventListener('click', () => {
+    landingScreen.style.transform = 'translateX(-100%)';
+    gameScreen.style.display = 'block'; 
+});
 
 const resultsDiv = document.getElementById('results');
 const scoreDiv = document.getElementById('score');
@@ -13,6 +21,11 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+const winSound = document.getElementById('win');
+winSound.playbackRate = 5;
+const loseSound = document.getElementById('lose');
+loseSound.playbackRate = 5;
 
 function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
@@ -25,9 +38,11 @@ function playRound(playerSelection) {
         (playerChoice === 'paper' && computerSelection === 'rock') ||
         (playerChoice === 'scissors' && computerSelection === 'paper')
     ) {
+        winSound.play();
         resultsDiv.textContent = `You Win! ${playerChoice} beats ${computerSelection}`;
         userScore ++;
     } else {
+        loseSound.play();
         resultsDiv.textContent = `You Lose! ${computerSelection} beats ${playerChoice}`;
         computerScore ++;
     }
@@ -37,26 +52,35 @@ function playRound(playerSelection) {
 
 function updateScore() {
     scoreDiv.textContent = `User: ${userScore} | Computer: ${computerScore}`;
-    
 
     if (userScore === 5 || computerScore === 5) {
         endGame();
     } 
 }
 
+const endWinSound = document.getElementById('end-win');
+const endLoseSound = document.getElementById('end-lose');
+
+
 function endGame() {
     if (userScore === 5) {
-        resultsDiv.textContent = "You win the game!";
+        endWinSound.play();
+        resultsDiv.innerHTML = '<span style="background-color: green; color: white; font-weight: bold; letter-spacing: 2px">YOU WIN THE GAME!</span>';
         document.getElementById('winGif').style.display = 'block';
     } else if (computerScore === 5) {
-        resultsDiv.textContent = "You lose the game!";
+        endLoseSound.play();
+        resultsDiv.innerHTML = '<span style="background-color: red; color: white; font-weight: bold; letter-spacing: 2px">YOU LOSE THE GAME!</span>';
         document.getElementById('loseGif').style.display = 'block';
     }
 
-    resetButton.style.display = 'block';
-    gameButtons.forEach((button) => {
-        button.style.display = 'none';
+    
+    gameButtons.forEach((div) => {
+        div.style.cssText = 'display: none !important;';
     });
+
+    document.getElementsByTagName('h1')[1].style.cssText = 'display:none';
+
+    resetButton.style.display = 'block';
 
     resetButton.addEventListener('click', ()=> {
         alert("Game has been reset. Play again ?");
@@ -69,17 +93,17 @@ function endGame() {
         document.getElementById('winGif').style.display = 'none';
         document.getElementById('loseGif').style.display = 'none';
 
-        gameButtons.forEach((button) => {
-            button.style.display = 'block';
+        gameButtons.forEach((div) => {
+            div.style.display = 'block';
         });
+        document.getElementsByTagName('h1')[1].style.cssText = 'display:block';
     });
 }
 
 
-gameButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+gameButtons.forEach((div) => {
+    div.addEventListener('click', (e) => {
         const playerSelection = e.target.id;
         playRound(playerSelection);
     });
 });
-
